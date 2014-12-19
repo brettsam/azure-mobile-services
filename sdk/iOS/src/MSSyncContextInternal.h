b2899e7cf7733afccc80478769fd802875f9669b
brettsam <brettsam@microsoft.com>
2014-12-19 11:37:18 -0800

    adding force push

--- sdk/iOS/src/MSSyncContextInternal.h
+++ sdk/iOS/src/MSSyncContextInternal.h
@@ -27,7 +27,9 @@
 
 -(void) pullWithQuery:(MSQuery *)query queryId:(NSString *)queryId completion:(MSSyncBlock)completion;
 
--(void) purgeWithQuery:(MSQuery *)query queryId:(NSString *)queryId force:(BOOL)force completion:(MSSyncBlock)completion;
+-(void) purgeWithQuery:(MSQuery *)query completion:(MSSyncBlock)completion;
+
+-(void) forcePurgeWithTable:(MSSyncTable *)syncTable completion:(MSSyncBlock)completion;
 
 
 #pragma mark * Operation Helpers
